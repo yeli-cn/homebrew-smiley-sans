3283c494ebf0880ab02da863fc4851be68903392
YeLi <i@yeli.io>
2022-11-28 22:04:07 +0800

    first commit.

---
+++ Formula/font-smiley-sans.rb
@@ -0,0 +1,11 @@
+cask "font-smiley-sans" do
+  version "1.0.0"
+  sha256 :no_check
+
+  url "https://github.com/atelier-anchor/smiley-sans/releases/download/v#{version}/smiley-sans-#{version}.zip",
+      verified: "github.com"
+  name "Smiley Sans"
+  homepage "https://atelier-anchor.com/typefaces/smiley-sans/"
+
+  font "smiley-sans-v#{version}/SmileySans-Oblique.otf"
+end
