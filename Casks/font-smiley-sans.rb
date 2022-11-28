375e0e2db345cc720ffcb6dcf200d22a75891bcd
YeLi <i@yeli.io>
2022-11-28 22:29:08 +0800

    add losing 'v' in url

--- Casks/font-smiley-sans.rb
+++ Casks/font-smiley-sans.rb
@@ -2,7 +2,7 @@ cask "font-smiley-sans" do
   version "1.0.0"
   sha256 :no_check
 
-  url "https://github.com/atelier-anchor/smiley-sans/releases/download/v#{version}/smiley-sans-#{version}.zip",
+  url "https://github.com/atelier-anchor/smiley-sans/releases/download/v#{version}/smiley-sans-v#{version}.zip",
       verified: "github.com"
   name "Smiley Sans"
   homepage "https://atelier-anchor.com/typefaces/smiley-sans/"
